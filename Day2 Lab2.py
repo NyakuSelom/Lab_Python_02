8422c09c1d56950c9f3c9c42c3029fff016f54e9
AITI Student <aiti-student@AITI-83.(none)>
2012-06-20 16:10:01 +0000

    lab on loops

---
+++ Day2 Lab2.py
@@ -0,0 +1,71 @@
+
+#EX 1
+"""
+#a
+x=2
+y=5
+"""
+
+"""
+#b
+x= 3
+y =1
+"""
+
+"""
+#c
+x=1
+y=1
+"""
+
+"""#d
+x=4
+y=3
+if x>2:
+    if y>2:
+        z=x+y
+        print "z is ",z
+else:
+    print "x is ",x"""
+
+#Ex 2
+"""
+#a
+x=3
+y=5
+"""
+
+"""
+#b
+x=2
+y=5
+"""
+"""
+z = x+3
+if z==1:
+    y=0
+elif z==2:
+    y=10
+elif z==4:
+    y+=1
+else:
+    y=1
+print y
+"""
+
+
+#Exc 3
+"""
+i = 0
+while i<10:
+    i+=1
+    if i%2 == 0:
+        print i
+"""
+
+#Ex 4
+i = 0
+while i > 10:
+    i+=1
+    if i%2 == 0:
+        print i
---
+++ UsingControlStructures.py
@@ -0,0 +1,71 @@
+#EX 5
+"""
+theInput = raw_input("Enter an integer")
+number = int(theInput)
+if number % 2 == 0:
+    print 'number is even'
+else:
+    print 'number is odd'
+"""
+
+#EX 6
+"""
+primarySchoolAge = 4
+legalVotingAge = 18
+ageForPresident = 50
+offRetirementAge = 65
+
+personAge = input('Enter an age: ')
+age = int(personAge)
+
+if  age < primarySchoolAge :
+    print"too young"
+
+if age >= legalVotingAge:
+    print "Remember to vote"
+
+if age >= ageForPresident:
+    print "Vote for me"
+else :
+    print "You can't be president"
+
+if age > offRetirementAge:
+    print" too old"
+"""
+
+#EX 7
+"""
+i=40
+while i >0:
+    i=i-1
+    print i
+    i= i-2
+"""
+
+#Ex 8
+"""
+for number in range (6,30):
+    if (number % 2 == 1) and(number % 3!=0) and (number % 5 != 0):
+        print number
+ """       
+        
+#Ex 9
+"""
+n=1
+while n<97:
+    if (79*n)%97==1:
+        print " ",n
+       break
+    n=n+1
+"""
+
+
+
+
+
+
+
+
+
+
+
